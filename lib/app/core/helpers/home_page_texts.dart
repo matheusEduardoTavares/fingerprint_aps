bdbc07e8cbe43ae01728352e9e7a365bdf3ba8b6
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-09-29 22:04:35 -0300

    Arrumado os textos de acordo com o nivel

--- lib/app/core/helpers/home_page_texts.dart
+++ lib/app/core/helpers/home_page_texts.dart
@@ -1,7 +1,24 @@
 class HomePageTexts {
   HomePageTexts._();
 
-  static const firstPermission = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consequat tellus id varius faucibus. In hac habitasse platea dictumst. Nunc convallis massa nec est vulputate congue. Aenean consequat maximus lacus, at feugiat odio dignissim non. Morbi pretium risus lectus, sit amet condimentum mauris vehicula sit amet. Cras consectetur est eget leo mollis, eu posuere arcu commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra in nunc eget scelerisque. Nulla placerat tristique dui vel rhoncus. Fusce vitae condimentum lorem.';  
-  static const secondPermission = 'Nam dignissim ac elit eget iaculis. Sed dui felis, ultricies vel lorem sed, venenatis semper dolor. Etiam eget purus rutrum, bibendum orci a, vehicula sem.';  
-  static const thirdPermission = 'Vivamus placerat dolor non eros varius, non tincidunt metus tempor. Nam gravida facilisis tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam quam metus, tempor et velit eget, auctor ullamcorper mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse luctus nibh eget vestibulum elementum. Morbi ex velit, facilisis vitae erat nec, maximus tincidunt libero. Pellentesque non ultricies ipsum. In hac habitasse platea dictumst. Quisque in ornare mi, eget pulvinar nibh. Mauris in augue luctus, posuere odio eu, cursus urna. Pellentesque vestibulum erat et lectus commodo dapibus ac non lectus.';  
+  static const firstPermission = 
+    'A utilização de Agrotóxico no Brasil, é o segundo maior causador de contaminaçãos nos rios, perdendo apenas para o esgoto doméstico.\n'
+    'No ano de 2012 utilizava-se 19% de todo o defensivo agricola produzido no planeta, e onde 99% dos venenos que são utilizados em lavouras acabam por não atingir a praga alvo, indo assim para os rios, solo e para a água subterranea.\n'
+    'Assim sempre que detectado o aparecimemento de agrotóxicos nas reservas de àgua subterranea , precisamos estar tomando todas as medidas para evitar o agravemento do problema principalmente nos rios e mares onde sempre é detectada uma quantidade maior de residuos de agrotóxicos.\n'
+    'O problema se dá quando a agua pe contaminada por defensivos agricolas onde a detecção é mais dificil , para estar tomando as devidas medidas administrativas e para estar removendo as substancias da agua.';
+
+  static const secondPermission = 
+    'Sendo o estado de São Paulo um grande produtor de uma vasta variedade de culturas e de agricultura , ele também é o responsabel por um consumo de 70\$ do consumo de àgua no Brasil.\n'
+    'Onde esse recurso é vital para a atividade agricula, mas nem por isso deixa de estar degradando tal.\n'
+    'A poluição hidrica que é causada no estado por produção agrícolas insustentável, é totalmente marcada pelo abuso na utilização de agrotóxicos e de outros produtos agroquímicos.\n'
+    'Produtos esses que ao serem utilizados acabam parando em rios lagos e nas reservas subterrâneas , fazendo um expoente crescimento na poluição das aguas do nosso estado e consequentemente do nosso pais.\n'
+    'A ONU alerta atráves de um relatório onde o número de pessoas sendo infectadas por uma água de má qualidade, em decorrencia do uso de agrotóxicos só vem aumentando, e preocupando a organização que está com os olhos voltados para o instituto internacional de gestão de água para averiguar quais ações serão tomadas pelo governo para a diminuição de tal problema.';
+
+  static const thirdPermission = 
+    'A melhor técnica para a contenção e diminuição do uso de agrotóxicos proibidos, será o manejo integrado de pragas, onde esse projeto consiste em utilizar recursos naturais para propor soluções para o desuso de agrotóxicos, sendo essas ações mais rentáveis para a agricultura.\n'
+    'Os insetos continuam nas lavouras mas não oferencendo risco a produção, mas ocorrendo isso apenas com o acompanhamento constante do controle das pragas para ter uma maior eficacia.\n'
+    'Esse método é dividido em três etapas básicas: avaliação do agroecossistema, tomada de decisão e seleção dos métodos de controle a serem adotados.\n'
+    'Essa primeira etapa se dá em determinar a densidade populacional das pragas por meio de amostragens ou monitoramento.\n'
+    'Segunda etapa, tomada de decisão, tendo como base os dados da primeira etapa, é feita a tomada de decisão de onde serão analisados os aspectos econômicos da cultura, e como está a relação de custo x beneficio do controle de pragas.\n'
+    'Terceira Etapa, seleção de métodos de controle, se for necessario tomar algum tipo de controle, o produtor deve optar por algum sistema que agregue mais de um método de controle.';
 }
